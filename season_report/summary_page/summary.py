f8f44398c45aa3a6b7a5e2c2ef007fdaf4a96e5a
PABSIRR <elobooster.help@gmail.com>
2025-06-02 14:28:13 -0700

    Started on summary.py, later job

--- season_report/summary_page/summary.py
+++ season_report/summary_page/summary.py
@@ -36,20 +36,40 @@ class gen:
         self.combined_data_settings = pd.read_excel(f'../../data/mens/{player}/combined.xlsx', sheet_name='Settings')
         
         self.results = None
+        self.uclaresults = None
     
     def getmatches(self):
         data = mens_results[(mens_results['Player1'] == self.player) | (mens_results['Player2'] == self.player)]
         data = data[data['Event Name'].str.startswith(('Dual Match', '2024 ITA', '2024-25 NCAA Division'))]
         self.results = data.reset_index()
+
+        ucla = pd.read_csv('../../data/mens/matches_2025.csv')
+        ucla['player1_name'] = ucla['player1_name'].str.title()
+        ucla['player2_name'] = ucla['player2_name'].str.title()
+        ucla['match_winner'] = ucla['match_winner'].str.title()
+        ucla = ucla[(ucla['player1_name'] == player_name) | (ucla['player2_name'] == player_name)]
+        self.uclaresults = ucla
     
-    def getdetailedmatches(self):
+    def longest_rally(self):
+        # Error Check
+        if "Shot" not in data.columns:
+            raise ValueError("The column 'Shot' was not found in the 'Shots' sheet.")
+        
+        # Find the index of the max shot value
+        max_rally_length = data["Shot"].max()  
+
+        # Return the value of the max shot
+        return max_rally_length
+    
+    def average_court_time(self):
+        pass
         
 
 
 
 # Create an instance of the class to run functions that you want!
 players = ['Rudy Quan', 'Emon Van Loben Sels', 'Kaylan Bigun', 'Alexander Hoogmartens', 
-             'Spencer Johnson', 'Aadarsh Tripathi', 'Giacomo Revelli', 'Gianluca Ballotta']
+           'Spencer Johnson', 'Aadarsh Tripathi', 'Giacomo Revelli', 'Gianluca Ballotta']
 p_map = {
     ''.join([name.split()[0][0], name.split()[-1][0]]): name
     for name in players
@@ -59,5 +79,5 @@ instance = gen(PLAYER)
 instance.getmatches()
 
 # Run functions
-
+print(instance.results)
 
